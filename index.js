e6c5dfeae00422fe93c1bf57c3ba6a5250931a92
asepwhite <septriyan.akbar@gmail.com>
2017-10-12 17:33:33 +0700

    impleent tugas 1

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const request = require('request');
+const rp = require('request-promise');
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('sisdis', 'root', 'rootroot', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+const User = sequelize.define('users', {
+  npm: { type: Sequelize.INTEGER,  unique: true },
+  nama: Sequelize.STRING,
+  saldo: Sequelize.INTEGER
+});
+
+// app.get('/createUser', function(req, res){
+//   sequelize.sync()
+//   .then(() => User.create({
+//     npm: 1406623064,
+//     nama: 'akbar',
+//     saldo: 0
+//   }))
+// });
 
 app.post('/ewallet/ping', function(req, res){
   responseObject = {};
@@ -13,6 +36,68 @@ app.get('/hello', function(req, res){
   console.log('hello');
 });
 
+app.post('/ewallet/register', jsonParser, function(req, res){
+    var responseObject = {};
+    responseObject.status_register = 0;
+    try {
+      if((req.body.user_id && req.body.nama)){
+          sequelize.sync().then(function(){
+            User.create({
+              npm: req.body.user_id,
+              nama: req.body.nama,
+              saldo: 0
+            }).then(function(){
+              register.status_register = 1;
+              res.send(responseObject);
+            }).catch(function(err){
+              responseObject.status_register = 4;
+              res.status(400).send(responseObject);
+            });
+          }).catch(function(err){
+            responseObject.status_register = 4;
+            res.status(400).send(responseObject);
+          });
+      } else {
+        responseObject.status_register = 99;
+        res.status(400).send(responseObject);
+      }
+    } catch(e) {
+      console.log(e);
+    }
+});
+
+app.post('/ewallet/getSaldo', jsonParser, function(req, res){
+    var responseObject = {};
+    responseObject.status_register = 0;
+    try {
+      if((req.body.user_id)){
+          sequelize.sync().then(function(){
+            // Project.findOne({ where: {user_id : 'aProject'} }).then(project => {
+            // })
+            User.create({
+              npm: req.body.user_id,
+              nama: req.body.nama,
+              saldo: 0
+            }).then(function(){
+              register.status_register = 1;
+              res.send(responseObject);
+            }).catch(function(err){
+              responseObject.status_register = 4;
+              res.status(400).send(responseObject);
+            });
+          }).catch(function(err){
+            responseObject.status_register = 4;
+            res.status(400).send(responseObject);
+          });
+      } else {
+        responseObject.status_register = 99;
+        res.status(400).send(responseObject);
+      }
+    } catch(e) {
+      console.log(e);
+    }
+});
+
 app.get('/list', function(req, res){
   var options = {
    root: __dirname,
@@ -22,40 +107,79 @@ app.get('/list', function(req, res){
 
 function checkQuorum(){
   var successPing = 0;
-  makePingRequest('http://127.0.0.1:3000/ewallet/ping', function(pingSuccess){
-    successPing += pingSuccess;
-  });
-  request.get('http://127.0.0.1:3000/list', function(error, response, body){
-      try{
-        listOfIP = JSON.parse(body);
-        for (var index in listOfIP) {
-          npmIpPair = JSON.parse(listOfIP[index]);
-        }
-      } catch(e){
-        return "error when call list.php";
-      }
-  })
-  // return successPing;
-}
+  var totalPing = 0;
+  var jobs = [];
+  var job = "";
+  var options = {
+    method: 'GET',
+    uri: 'http://127.0.0.1:3000/list',
+    body: {},
+    json: true // Automatically stringifies the body to JSON
+ };
+ var job = "";
 
-function makePingRequest(requestUrl, cb) {
-  var pingSuccess = 0;
-  return request.post({url:requestUrl, formData:{}}, function(error, response, body){
-    try{
-      var responseObject = JSON.parse(body);
-      if(responseObject.pong === 1){
-        pingSuccess = 1;
-      }
-      cb(1);
-    } catch(e){
-      return "error when call self ping";
-    }
-  });
+ return rp(options).then(function(body){
+  //call other student ping service asynchronously
+   try {
+     arrayOfIP = body;
+     options.method = 'POST';
+     for (var index in arrayOfIP) {
+       pairNpmIp = arrayOfIP[index];
+       if(pairNpmIp.npm === '1406578275'){
+         options.uri = 'http://127.0.0.1:3000/ewallet/ping';
+         job = rp(options).then(function (body) {
+              if(body.pong === 1){
+                successPing += 1;
+              }
+         }).catch(function (err) {});
+         jobs.push(job);
+         totalPing+= 1;
+       } else if (pairNpmIp.npm === '1406543813') {
+         options.uri = 'http://127.0.0.1:3000/ewallet/ping';
+         job = rp(options).then(function (body) {
+              if(body.pong === 1){
+                successPing += 1;
+              }
+         }).catch(function (err) {});
+         jobs.push(job);
+         totalPing+= 1;
+       } else if (pairNpmIp.npm === '1406543832') {
+         options.uri = 'http://127.0.0.1:3000/ewallet/ping';
+         job = rp(options).then(function (body) {
+              if(body.pong === 1){
+                successPing += 1;
+              }
+         }).catch(function (err) {});
+         jobs.push(job);
+         totalPing+= 1;
+       }
+     }
+     //call self ping
+     job = rp(options).then(function (body) {
+          if(body.pong === 1){
+            successPing += 1;
+          }
+     }).catch(function (err) {});
+     totalPing+= 1;
+     jobs.push(job);
+
+     return Promise.all(jobs).then(function(){
+       var resultObject = {}
+       resultObject.successPing = successPing;
+       resultObject.totalPing = totalPing;
+       return Promise.resolve(resultObject);
+     });
+
+   } catch(e) {
+     console.log(e);
+   }
+ }).catch(function (err) {});
 }
 
 app.get('/ewallet/checkQuorum', function(req, res){
-  var pingSuccess = checkQuorum();
-  res.send(""+pingSuccess);
+  checkQuorum().then(function(result){
+    res.send(result);
+  });
 });
 
 app.listen(3000, function () {
